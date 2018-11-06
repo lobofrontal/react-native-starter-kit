a28308b2948db22c2da1b1b91a37d5e31d7a8b50
Lobo Frontal <matheus.veras@lobofrontal.com.br>
2018-11-06 09:19:25 -0300

    refactor(Welcome) file struture and add readme

--- src/screens/welcome/index.js
+++ src/screens/welcome/index.js
@@ -1,18 +1,2 @@
-// Core
-import React, { Component } from 'react';
-import { Page } from '../../components';
-import styles from './styles';
-import {
-    Text,
-  } from 'react-native';
-class Welcome extends Component {
-  render() {
-    return (
-      <Page style={styles.container}>
-        <Text>Hello World</Text>
-      </Page>
-    );
-  }
-}
-
-export default Welcome;
+import View from './welcome';
+export default View;
