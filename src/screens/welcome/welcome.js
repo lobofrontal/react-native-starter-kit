5bf667e6956d940cce2245f0886aed952af7f094
Lobo Frontal <matheus.veras@lobofrontal.com.br>
2018-11-06 12:24:26 -0300

    feat(Animeted) enable animeted view

--- src/screens/welcome/welcome.js
+++ src/screens/welcome/welcome.js
@@ -3,22 +3,35 @@ import React, { Component } from 'react';
 import { Page } from '../../components';
 import styles from './styles';
 import { FadeUp, FadeDown } from '../../tools';
+import { connect } from 'react-redux';
+
 import {
     Text,
   } from 'react-native';
 class Welcome extends Component {
+  componentDidMount = () => {
+    setTimeout(() => this.props.setInit(true), 200);
+  };
+
   render() {
     return (
       <Page style={styles.container}>
-        <FadeUp pose={'visible'}>
+        <FadeUp pose={this.props.init ? 'visible' : 'hidden'}>
           <Text>Hello World</Text>
         </FadeUp>
-        <FadeDown pose={'visible'}>
+        <FadeDown pose={this.props.init ? 'visible' : 'hidden'}>
           <Text>Exemplo FadeDow</Text>
         </FadeDown>
       </Page>
     );
   }
 }
+const mapStateToProps = ({ initial, global }) => ({
+  init: initial.init,
+});
 
-export default Welcome;
+const mapDispatchToProps = ({ initial, global }) => ({
+  setInit: payload => initial.setInit(payload),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Welcome);
